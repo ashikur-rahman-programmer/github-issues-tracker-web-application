63d6a63f5822dbebd48b481e036929abb12c4d11
ashik081 <freelancerashik81@gmail.com>
2026-03-07 23:53:36 +0600

    added modal dynamicly

--- script/script.js
+++ script/script.js
@@ -112,6 +112,11 @@ const removeLoadingSpinner = () => {
   spinnerLoad.classList.add("hidden");
 };
 
+// show spinner for modal
+const showModalSpinner = () => {
+  spinnerLoad.classList.remove("hidden");
+};
+
 // labels arr output
 const labelFunc = (arr) => {
   const specificElm = arr.map((elm) => {
@@ -161,14 +166,69 @@ const borderTopCon = (bor) => {
   }
 };
 
-// get date
-
+// get dateUpdate
 const dateUpdate = (date) => {
   const createDate = new Date(date);
   const day = createDate.getDate();
   const month = createDate.getMonth() + 1;
   const year = createDate.getFullYear();
   return `${day} / ${month} / ${year}`;
+};
+
+// modal option start
+// issuesModal.addEventListener("click", () => {});
+
+// load modal
+const loadModal = async (id) => {
+  showModalSpinner();
+  const res = await fetch(
+    `https://phi-lab-server.vercel.app/api/v1/lab/issue/${id}`,
+  );
+  const data = await res.json();
+  displayModal(data.data);
+  removeLoadingSpinner();
+};
+
+// displayModal
+const displayModal = (card) => {
+  const modalContainer = document.getElementById("modal-card");
+  modalContainer.innerHTML = `
+ 
+                  <h2 class="text-xl font-bold">${card.title}</h2>
+                  <div class="flex items-center gap-2">
+                    <span  class="badge ${card.status === "open" ? "badge-success" : "badge-warning"} rounded-2xl">${card.status.toUpperCase()}</span>
+                    <p class="text-gray-500 flex items-center gap-2">
+                      <img src="./assets/Ellipse 5.png" alt="" />Opened by ${card.assignee}
+                    </p>
+                    <p class="text-gray-500 flex items-center gap-2">
+                      <img src="./assets/Ellipse 5.png" alt="" />${dateUpdate(card.createdAt)}
+                    </p>
+                  </div>
+                </div>
+                <div class="flex text-[12px] flex-wrap items-center gap-2">
+                  ${labelFunc(card.labels)}
+                </div>
+                <p class="text-gray-500 line-clamp-2">
+                  ${card.description}
+                </p>
+
+                <div class="grid grid-cols-2 px-4 py-2 bg-gray-100 rounded-xl">
+                  <div class="space-y-2">
+                    <p class="text-lg text-gray-500">Assignee:</p>
+                    <p class="font-semibold">${card.assignee}</p>
+                  </div>
+                  <div class="space-y-2">
+                    <p class="text-lg text-gray-500">Priority:</p>
+                    <span
+                      id="priority-unq"
+                      class="rounded-4xl py-1 px-3 ${priorityFunc(card.priority)} bg-gray-100"
+                    >
+                      ${card.priority.toUpperCase()}
+                    </span>
+                  </div>
+               
+  `;
+  document.getElementById("issues_modal").showModal();
 };
 
 //cards  dynamic loaded here
@@ -196,6 +256,7 @@ const displayCard = (cards) => {
 
     const div = document.createElement("div");
     div.className = `space-y-5 p-4 shadow-md rounded-lg ${borderTopCon(card.status)}`;
+    div.addEventListener("click", () => loadModal(card.id));
     div.innerHTML = `
     
         <div class="space-y-5 pb-4 border-b-2 border-b-gray-200">
